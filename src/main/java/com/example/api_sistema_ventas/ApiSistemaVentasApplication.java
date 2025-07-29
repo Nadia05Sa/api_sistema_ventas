027270d8479802a690d82d5c28d32f47c9d4fa46
Nadia05Sa <168792799+Nadia05Sa@users.noreply.github.com>
2025-07-29 16:12:26 -0600

    proyecto creado

---
+++ src/main/java/com/example/api_sistema_ventas/ApiSistemaVentasApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_sistema_ventas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSistemaVentasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiSistemaVentasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/api_sistema_ventas/ApiSistemaVentasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_sistema_ventas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSistemaVentasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
